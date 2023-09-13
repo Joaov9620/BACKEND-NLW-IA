9bfb224b564b40f2120115983605087dae465f68
joao <pjoaovictor22@hotmail.com>
2023-09-13 00:35:38 -0300

    feat: install cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors' //determina quais endereços vão conectar a api
 import { getAllPromptsRoute } from './routes/get-all-prompts'
-import { register } from 'module'
 import { uploadVideoRoute } from './routes/upload-video'
 import { createTranscriptionRoute } from './routes/create-transcription'
 import { generateAICompletionRoute } from './routes/generate-ai-completion'
 
 const app = fastify()
+
+//qualquer frontend pode acessar a api
+app.register(fastifyCors,{
+  origin:'*',
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
